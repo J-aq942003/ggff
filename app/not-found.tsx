266a14e9b6185b7f8194768e82565421fc4ce013
jha399 <137772858+J-aq942003@users.noreply.github.com>
2025-03-21 19:52:23 +0200

    Modify notfound.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,12 @@
 //import { usePathname } from "next/navigation";
 import React from "react";
+export const dynamic = 'force-static'  // Add this line
 const notFound = () => {
   //const pathName = usePathname();
   return (
     <div className="">
       <p>
-        Not Found The Page in path <i className="font-boldf">{/*pathName*/}</i>
+        Not Found The Page in path
       </p>
     </div>
   );
