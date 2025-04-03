625a8e98ef0f691abc387192adcd0dd262cac206
jha399 <137772858+J-aq942003@users.noreply.github.com>
2025-04-03 09:15:26 +0300

    Update3 middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -13,9 +13,9 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   // const { userId, redirectToSignIn } = await auth();
-  /*if (isProtectedRoute(req)) {
+  if (isProtectedRoute(req)) {
     await auth.protect();
-  }*/
+  }
   // if (!userId && isProtectedRoute(req)) {
   //   // Add custom logic to run before redirecting
   //   console.log("route is: " + req.url);
@@ -29,6 +29,5 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
-  ],
-  runtime: "nodejs", // Force Node.js runtime
+  ],  
 };
